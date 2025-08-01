bef003e859571ddaef9543cad7a7656816d55759
nghgphuo <phuocnguyen36398@gmail.com>
2025-08-01 22:45:58 +0700

    Call API list product in home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,23 +4,32 @@ import styles from './styles.module.scss';
 import AdvanceHeadling from '@components/AdvanceHeadling/AdvanceHeadling';
 import Info from '@components/Info/Info';
 import HeadingListProduct from '@components/HeadingListProduct/HeadingListProduct';
+import { getProducts } from '@/apis/productsService';
+import { useEffect, useState } from 'react';
+import PopularProduct from '@components/PopularProduct/PopularProduct';
 
 function HomePage() {
-  const { container } = styles;
+  const [listProducts, setListProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((res) => {
+      setListProducts(res.contents);
+    });
+  }, []);
+
   return (
     <>
-      <div className={container}>
-        <MyHeader />
-        <Banner />
-        <Info />
-        <AdvanceHeadling />
-        <HeadingListProduct />
-        <div
-          style={{
-            height: '200px'
-          }}
-        ></div>
-      </div>
+      <MyHeader />
+      <Banner />
+      <Info />
+      <AdvanceHeadling />
+      <HeadingListProduct data={listProducts.slice(0, 2)} />
+      <PopularProduct data={listProducts.slice(2, listProducts.length)} />
+      <div
+        style={{
+          height: '200px'
+        }}
+      ></div>
     </>
   );
 }
