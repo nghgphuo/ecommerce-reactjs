cf9ed842d00fd7637e7cb4a26f6dfc0daebc9120
nghgphuo <phuocnguyen36398@gmail.com>
2025-08-04 18:26:56 +0700

    add react toast message

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import SideBar from '@components/Sidebar/Sidebar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routers from '@/routers/routers';
 import { Suspense } from 'react';
+import { ToastProvider } from '@/contexts/ToastProvider';
 
 function App() {
   return (
-    <SidebarProvider>
-      <SideBar />
+    <ToastProvider>
+      <SidebarProvider>
+        <SideBar />
 
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {routers.map((item, index) => {
-              return (
-                <Route
-                  path={item.path}
-                  element={<item.component />}
-                  key={index}
-                />
-              );
-            })}
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </SidebarProvider>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              {routers.map((item, index) => {
+                return (
+                  <Route
+                    path={item.path}
+                    element={<item.component />}
+                    key={index}
+                  />
+                );
+              })}
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </SidebarProvider>
+    </ToastProvider>
   );
 }
 export default App;
