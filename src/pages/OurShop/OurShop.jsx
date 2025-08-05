6068d3587b0841e2084449df84795aa8ce596ef3
nghgphuo <phuocnguyen36398@gmail.com>
2025-08-05 22:08:28 +0700

    render product size and choose product size

--- src/pages/OurShop/OurShop.jsx
+++ src/pages/OurShop/OurShop.jsx
@@ -6,6 +6,7 @@ import Banner from '@pages/OurShop/components/Banner';
 import { OurShopProvider } from '@contexts/OurShopProvider';
 import Filter from '@pages/OurShop/components/Filter';
 import ListProducts from '@pages/OurShop/components/ListProducts';
+import MyFooter from '@components/Footer/Footer';
 
 function OurShop() {
   const { container, functionBox, specialText, btnBack } = styles;
@@ -36,6 +37,7 @@ function OurShop() {
           <ListProducts />
         </div>
       </MainLayout>
+      <MyFooter />
     </OurShopProvider>
   );
 }
--- src/pages/OurShop/components/ListProducts.jsx
+++ src/pages/OurShop/components/ListProducts.jsx
@@ -4,34 +4,44 @@ import { OurShopContext } from '@/contexts/OurShopProvider';
 import ProductItem from '@components/ProductItem/ProductItem';
 import styles from '../styles.module.scss';
 import ProductItemLine from '@components/ProductItem/ProductItemLine';
+import Button from '@components/Button/Button';
 
 function ListProducts() {
   const { containerProduct } = styles;
-  const { products, isShowGrid } = useContext(OurShopContext);
+  const { products, isShowGrid, isLoading } = useContext(OurShopContext);
 
   return (
     <>
       <MainLayout>
-        <div className={isShowGrid && containerProduct}>
-          {products.map((item) => (
-            <>
-              {' '}
-              {isShowGrid ? (
-                <ProductItem
-                  key={item.id}
-                  src={item.images[0]}
-                  prevSrc={item.images[1]}
-                  name={item.name}
-                  price={item.price}
-                  details={item}
-                  isNotHomePage
-                />
-              ) : (
-                <ProductItemLine details={item} />
-              )}
-            </>
-          ))}
-        </div>
+        {isLoading ? (
+          <>Loading...</>
+        ) : (
+          <>
+            <div className={isShowGrid && containerProduct}>
+              {products.map((item) => (
+                <>
+                  {isShowGrid ? (
+                    <ProductItem
+                      key={item.id}
+                      src={item.images[0]}
+                      prevSrc={item.images[1]}
+                      name={item.name}
+                      price={item.price}
+                      details={item}
+                      isNotHomePage
+                    />
+                  ) : (
+                    <ProductItemLine details={item} />
+                  )}
+                </>
+              ))}
+            </div>
+
+            <div style={{ width: '180px', margin: '50px auto' }}>
+              <Button content={'LOADING MORE PRODUCT'}></Button>
+            </div>
+          </>
+        )}
       </MainLayout>
     </>
   );
