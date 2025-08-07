0a52a4b4bb92e6e47dd674770356f7b0dda9e760
nghgphuo <phuocnguyen36398@gmail.com>
2025-08-07 22:11:29 +0700

    create steps section in detail cart

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,22 @@
+import MyFooter from '@components/Footer/Footer';
+import MyHeader from '@components/Header/Header';
+
+import styles from './styles.module.scss';
+import Steps from '@/pages/Cart/Components/steps/Steps';
+import Contents from '@/pages/Cart/Components/contents/Contents';
+
+function Cart() {
+  const { container } = styles;
+  return (
+    <>
+      <MyHeader />
+      <div className={container}>
+        <Steps />
+        <Contents />
+      </div>
+      <MyFooter />
+    </>
+  );
+}
+
+export default Cart;
